e73f73b95bec6690426eeccbcad57d565146107f
cristianccgg <cristianccggg@gmail.com>
2024-07-24 13:01:02 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
 import { Navbar } from "./components/Navbar";
 import "./App.css";
 import { Slide } from "./components/Slide";
