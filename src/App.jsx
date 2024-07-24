876648b54d29fe241e0e95089cc1343a915f1fd4
cristianccgg <cristianccggg@gmail.com>
2024-07-24 12:57:52 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import "./App.css";
 import { Slide } from "./components/Slide";
